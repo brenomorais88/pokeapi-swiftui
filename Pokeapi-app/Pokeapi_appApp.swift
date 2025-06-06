4e600c6b0566bbac47be7d47fba439c4b2d4e5cc
Breno Morais <brenomorais88@gmail.com>
2025-06-06 20:57:20 -0300

    first commit

---
+++ Pokeapi-app/Pokeapi_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokeapi_appApp.swift
+//  Pokeapi-app
+//
+//  Created by Breno Morais on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokeapi_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
